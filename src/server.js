097ab90e8a3cb349a8731329df2803cc023ef901
Mani <mani.sundar@paramantra.com>
2025-12-13 02:11:03 +0530

    port static value used

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
-const initSockets = require("./sockets"); // index.js inside sockets/
+const initSockets = require("./sockets");     // index.js inside sockets/
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
@@ -17,10 +14,10 @@ const server = http.createServer(app);
 // ---------------------------
 // 2. Initialize Socket.IO
 // ---------------------------
-initSockets(server);
+initSockets(server);  
 // This attaches socket to your server (driver tracking, chat, etc)
 
 // ---------------------------
 // 3. Start Server
 // ---------------------------
-server.listen(PORT, HOST, () => console.log("Server is listening..."));
+server.listen(3000, '127.0.0.1', () => console.log('server is listening...'));
